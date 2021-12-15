562a0273607a299f0a86a2b3ad35481171d42081
AnkushSupnar <ankushsupnar@gmail.com>
2021-12-15 23:02:20 +0530

    Commit on 15 December 2021

---
+++ src/main/java/com/ankush/controller/transaction/PurchaseInvoiceController.java
@@ -0,0 +1,14 @@
+package com.ankush.controller.transaction;
+
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Component;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class PurchaseInvoiceController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
